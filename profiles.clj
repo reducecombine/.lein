23d5a1e3213c53a1ba2cd81f382cd5c1cc0f4a2c
vemv <vemv@users.noreply.github.com>
2018-11-27 20:56:22 +0100

    Delete DocSolver stuff

--- profiles.clj
+++ profiles.clj
@@ -11,7 +11,6 @@
                    :exclusions
                    [org.clojure/clojure]]]
         :jvm-opts ["-Dapple.awt.UIElement=true"]
-        :pedantic? :warn
         :how-to-ns {:require-docstring?      false
                     :sort-clauses?           true
                     :allow-refer-all?        false
@@ -24,33 +23,6 @@
                                                         :password #=(some-> "CLOJARS_PASSWORD"
                                                                             System/getenv
                                                                             eval)}}}
- :emacs-docsolver-backend {:repl-options {:init (do
-                                                  (require 'cider.nrepl.middleware.stacktrace)
-                                                  ;; https://github.com/clojure-emacs/cider-nrepl/issues/547
-                                                  (let [prev (-> *ns* ns-name)]
-                                                    (in-ns 'cider.nrepl.middleware.stacktrace)
-                                                    (eval '(defn flag-project
-                                                             [{:keys [ns] :as frame}]
-                                                             (if (and directory-namespaces ns
-                                                                      (or (contains? directory-namespaces (symbol ns))
-                                                                          (.startsWith ns "app.")
-                                                                          (.startsWith ns "unit.")))
-                                                               (flag-frame frame :project)
-                                                               frame)))
-                                                    (in-ns prev))
-                                                  (user/reset))
-                                          :port 45432
-                                          :timeout 180000}
-                           :source-paths ^:replace #=(some-> "DS_BACKOFFICE_BACKEND_DEV_SOURCE_PATHS"
-                                                             System/getenv
-                                                             read-string
-                                                             eval)
-                           :dependencies [[org.clojure/tools.nrepl "0.2.13" :exclusions [org.clojure/clojure]]]
-                           :plugins [[refactor-nrepl "2.4.0" :exclusions [org.clojure/tools.logging]]
-                                     [cider/cider-nrepl "0.16.0"]]}
- :emacs-docsolver-frontend {:dependencies [[lein-doo "0.1.10" :exclusions [org.clojure/clojure]]]
-                            :source-paths ["test" "../common/test"]
-                            :cljsbuild {:builds {:dev {:source-paths ["test" "../common/test"]}}}}
  :emacs-figwheel {:dependencies [[com.cemerick/piggieback "0.2.2"]
                                  [figwheel-sidecar "0.5.16"]]
                   :plugins [[cider/cider-nrepl "0.16.0"]]
