51752e2ca746aea431e287d63290c0b069918375
vemv <vemv@users.noreply.github.com>
2019-02-18 06:02:01 +0100

    Make :emacs-backend apt for all kind of projects

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,4 @@
 {:user {#_ #_ :repositories [["vemv" #=(eval (System/getenv "MYMAVENREPO_READ_URL"))]]
-        :dependencies [[nrepl-debugger "0.1.0-SNAPSHOT"]]
         :plugins [[lein-eftest "0.5.4"]
                   [lein-pprint "1.1.2" :exclusions [org.clojure/clojure]]
                   [lein-lein "0.2.0"]]
@@ -18,12 +17,24 @@
                               ["snapshots" :clojars]]
         :repository-auth {#"https://clojars\.org/repo" {:username "vemv"
                                                         :password #=(eval (System/getenv "CLOJARS_PASSWORD"))}}}
- ;; the following profile is only necessary when staring `lein repl` from iTerm:
- :emacs-backend {:dependencies [[org.clojure/tools.nrepl "0.2.13" :exclusions [org.clojure/clojure]]
+ ;; the following profile serves for two use cases:
+ ;; * Launching `lein repl` from iTerm
+ ;; * Launching an in-Emacs JVM
+ ;; Perhaps for the latter, the :plugins section is redundant. Hasn't given problems so far.
+ :emacs-backend {:dependencies [[formatting-stack "0.5.0"]
+                                [org.clojure/tools.nrepl "0.2.13" :exclusions [org.clojure/clojure]]
                                 [org.clojure/tools.namespace "0.3.0-alpha4"]
+                                [nrepl-debugger "0.1.0-SNAPSHOT"]
                                 [com.stuartsierra/component.repl "0.2.0"]]
                  :plugins [[refactor-nrepl "2.4.0" :exclusions [org.clojure/tools.logging]]
-                           [cider/cider-nrepl "0.16.0"]]}
+                           [cider/cider-nrepl "0.16.0"]]
+                 :repl-options {:init (do
+                                        (clojure.core/require 'net.vemv.nrepl-debugger)
+                                        (clojure.core/require 'formatting-stack.core)
+                                        (clojure.core/require 'clojure.tools.namespace.repl)
+                                        (clojure.core/require 'com.stuartsierra.component.repl)
+                                        (clojure.tools.namespace.repl/set-refresh-dirs "src" "test")
+                                        (clojure.tools.namespace.repl/refresh))}}
  :nedap-key {:source-paths ["specs/server"]
              :jvm-opts ["-Dlogback.configurationFile=resources/logback-no-stdout.xml"]
              :repl-options ^:replace {:port 41235
@@ -31,6 +42,7 @@
                                       :init-ns user
                                       :init (do
                                               (clojure.core/require 'net.vemv.nrepl-debugger)
+                                              (clojure.core/require 'formatting-stack.core)
                                               (clojure.core/require 'clojure.tools.namespace.repl)
                                               (clojure.core/require 'com.stuartsierra.component.repl)
                                               (clojure.tools.namespace.repl/set-refresh-dirs "dev/server" "src/server" "specs/server")
