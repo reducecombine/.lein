d15aa6b5039a347d3aaeb5cce0ecabbbd009c273
vemv <vemv@users.noreply.github.com>
2019-12-26 20:43:54 +0100

    Unify "refresh/reset on startup" snippets

--- profiles.clj
+++ profiles.clj
@@ -50,59 +50,84 @@
 
  :emacs-backend-init {:repl-options {:init {:emacs-backend-init
                                             (do
+                                              (clojure.core/require 'clojure.tools.namespace.repl)
+                                              (clojure.core/require 'com.stuartsierra.component.repl)
+                                              ;; the eval business prevents `refactor-nrepl.analyzer/warm-ast-cache` from throwing "not found"
+                                              (clojure.core/alter-var-root #'clojure.test/*load-tests* (clojure.core/constantly false))
                                               (clojure.core/eval '(clojure.core/create-ns 'vemv-warm))
-                                              (clojure.core/eval '(clojure.tools.namespace.repl/set-refresh-dirs "dev" "src" "test" "modules"))
+                                              (clojure.core/eval '(try
+                                                                    (require 'dev)
+                                                                    (catch Exception e
+
+                                                                      (try
+                                                                        (require 'user)
+                                                                        (catch Exception _))
+
+                                                                      (when-not (seq clojure.tools.namespace.repl/refresh-dirs)
+
+                                                                        ;; this refer is only useful if there's no :init-ns
+                                                                        (clojure.core/refer 'clojure.tools.namespace.repl
+                                                                                            :only
+                                                                                            '[clear refresh refresh-dirs set-refresh-dirs])
+
+                                                                        (->> ["dev" "libs" "modules" "src" "test"]
+                                                                             (filter (fn [^String x]
+                                                                                       (-> x java.io.File. .exists)))
+                                                                             (apply clojure.tools.namespace.repl/set-refresh-dirs))))))
                                               (clojure.core/eval '(clojure.core/intern 'vemv-warm
                                                                                        'vemv-warm
                                                                                        (clojure.core/delay
-                                                                                        (clojure.core/eval '(refactor-nrepl.analyzer/warm-ast-cache)))))
+                                                                                        (refactor-nrepl.analyzer/warm-ast-cache))))
                                               (clojure.core/eval '(clojure.core/intern 'vemv-warm
-                                                                                       'vemv-do-warm
-                                                                                       (fn []
-                                                                                         @vemv-warm/vemv-warm)))
-                                              (clojure.core/eval '(clojure.tools.namespace.repl/refresh :after 'vemv-warm/vemv-do-warm)))}}}
+                                                                                       'init-fn
+                                                                                       com.stuartsierra.component.repl/initializer))
+                                              (clojure.core/eval '(clojure.core/intern
+                                                                   'vemv-warm
+                                                                   'vemv-do-warm
+                                                                   (fn []
+                                                                     (if (eval '(clojure.core/= vemv-warm/init-fn
+                                                                                                com.stuartsierra.component.repl/initializer))
+                                                                       @vemv-warm/vemv-warm
+                                                                       (clojure.core/when-let [v (try
+                                                                                                   (eval '(com.stuartsierra.component.repl/reset))
+                                                                                                   (future ;; wrap in a future - it is assumed projects with a System can be large:
+                                                                                                     @vemv-warm/vemv-warm)
+                                                                                                   (catch java.lang.Throwable v
+                                                                                                     (clojure.core/when (clojure.core/instance? java.io.FileNotFoundException v)
+                                                                                                       (eval '(clojure.tools.namespace.repl/clear)))
+                                                                                                     (clojure.core/when (com.stuartsierra.component/ex-component? v)
+                                                                                                       (clojure.core/some-> v
+                                                                                                                            clojure.core/ex-data
+                                                                                                                            :system
+                                                                                                                            com.stuartsierra.component/stop))
+                                                                                                     v))]
+                                                                         (clojure.core/when (clojure.core/instance? java.lang.Throwable v)
+                                                                           (clojure.core/when (clojure.core/instance? java.io.FileNotFoundException v)
+                                                                             (eval '(clojure.tools.namespace.repl/clear)))
+                                                                           (clojure.core/-> v .printStackTrace)))))))
+
+                                              (clojure.core/eval '(clojure.tools.namespace.repl/refresh :after 'vemv-warm/vemv-do-warm))
+
+                                              ;; particularly useful for projects without a `t.n` setup whatsoever
+                                              (let [used (->> *ns* ns-refers keys set)]
+                                                (->> '[clear refresh refresh-dirs set-refresh-dirs]
+                                                     (remove used)
+                                                     (vec)
+                                                     (list 'clojure.tools.namespace.repl :only)
+                                                     (apply clojure.core/refer))))}}}
 
  :nedap-gw {:repl-options {:port 41237}}
 
  :nedap-link {:source-paths ["src/test"]
               :repl-options {:port 41236
                              :timeout 600000
-                             :init-ns dev
-                             :init {:nedap-link
-                                    (do
-                                      (eval '(clojure.tools.namespace.repl/set-refresh-dirs "src/dev" "src/main" "src/test"))
-                                      (eval '(clojure.tools.namespace.repl/refresh)))}}}
+                             :init-ns dev}}
 
  :nedap-key {:source-paths ["specs/server"]
              :jvm-opts ["-Dlogback.configurationFile=resources/logback-no-stdout.xml"]
              :repl-options {:port 41234
                             :timeout 600000
-                            :init-ns dev
-                            :init {:nedap-key
-                                   (do
-                                     (clojure.core/alter-var-root #'clojure.test/*load-tests* (clojure.core/constantly false))
-                                     (eval '(clojure.tools.namespace.repl/set-refresh-dirs "dev/server"
-                                                                                           "src/server"
-                                                                                           "src/shared"
-                                                                                           "specs/server"
-                                                                                           "pepkey-migrations"
-                                                                                           "modules"
-                                                                                           "libs"
-                                                                                           "tasks"))
-                                     (clojure.core/when-let [v (try
-                                                                 (eval '(com.stuartsierra.component.repl/reset))
-                                                                 (clojure.core/future
-                                                                   (eval '(refactor-nrepl.analyzer/warm-ast-cache)))
-                                                                 (catch java.lang.Throwable v
-                                                                   (clojure.core/when (clojure.core/instance? java.io.FileNotFoundException v)
-                                                                     (eval '(clojure.tools.namespace.repl/clear)))
-                                                                   (clojure.core/when (com.stuartsierra.component/ex-component? v)
-                                                                     (clojure.core/some-> v clojure.core/ex-data :system com.stuartsierra.component/stop))
-                                                                   v))]
-                                       (clojure.core/when (clojure.core/instance? java.lang.Throwable v)
-                                         (clojure.core/when (clojure.core/instance? java.io.FileNotFoundException v)
-                                           (eval '(clojure.tools.namespace.repl/clear)))
-                                         (clojure.core/-> v .printStackTrace))))}}}
+                            :init-ns dev}}
 
  :refactor-nrepl {:dependencies [[http-kit "2.3.0"]
                                  [cheshire "5.8.0"]
