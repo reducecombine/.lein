22a5e9f9e910dde8b453bf58781ad48b354c6693
vemv <vemv@users.noreply.github.com>
2021-03-13 20:48:56 +0100

    Implement `ns-sym->filename`

--- scripts/vemv.clj
+++ scripts/vemv.clj
@@ -1,7 +1,10 @@
 (ns vemv
   (:require
    [clojure.test]
-   [lambdaisland.deep-diff]))
+   [formatting-stack.linters.one-resource-per-ns]
+   [lambdaisland.deep-diff]
+   [clojure.string :as string]
+   [nedap.speced.def :as speced]))
 
 (defn is=
   "Performs `(is = ...)`, reporting the diff between `expected` and `actual` when the test assertion fails."
@@ -17,3 +20,13 @@
                                     (lambdaisland.deep-diff/pretty-print (lambdaisland.deep-diff/printer {:print-color false})))))
      (clojure.test/is (= expected actual)
                       desc))))
+
+
+(speced/defn ns-sym->filename* [^simple-symbol? ns-sym]
+  (some-> (list 'ns ns-sym)
+          (formatting-stack.linters.one-resource-per-ns/ns-decl->resource-path ".clj")
+          formatting-stack.linters.one-resource-per-ns/resource-path->filenames
+          first
+          (string/replace "file:" "")))
+
+(def ns-sym->filename (memoize ns-sym->filename*))
