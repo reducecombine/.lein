d22c983c7735b67de5c3dca44c3e624b56955dd9
vemv <vemv@users.noreply.github.com>
2021-07-31 05:21:26 +0200

    Add guava
    A recent version tends to avoid conflicts.

--- profiles.clj
+++ profiles.clj
@@ -97,6 +97,7 @@
                                        [medley "1.2.0"]
                                        [mvxcvi/puget "1.1.1"]
                                        [fipp "0.6.23"]
+                                       [com.google.guava/guava "25.1-jre"]
                                        [mvxcvi/arrangement "1.2.1"]
                                        [nrepl-debugger "0.1.0-SNAPSHOT" :exclusions [nrepl]]
                                        [org.clojure/clojure "1.10.2"]
