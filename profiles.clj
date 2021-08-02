8e94e0828a2f76a38d6fd2b2e6e7f2b1e17495b1
vemv <vemv@users.noreply.github.com>
2021-08-02 13:26:41 +0200

    Add `lein-collisions`

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,5 @@
 {:user               {:plugins                  [[com.github.liquidz/antq "RELEASE"]
+                                                 [lein-collisions "0.1.4"]
                                                  [lein-pprint "1.1.2"]
                                                  [lein-subscribable-urls "0.1.0-alpha2"]
                                                  [threatgrid/lein-lean "0.6.0"]
