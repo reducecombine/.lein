2f278779c37be9c6dced2b8a814a9fa374890c54
vemv <vemv@users.noreply.github.com>
2022-07-28 05:13:16 +0200

    Add `:eastwood-ci*` profiles

--- profiles.clj
+++ profiles.clj
@@ -195,4 +195,9 @@
 
  :eftest             {:plugins [[lein-eftest "0.5.8"]]
                       :eftest  {:multithread? false
-                                :fail-fast?   true}}}
+                                :fail-fast?   true}}
+
+ :eastwood-ci-clojure-1-10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+
+ :eastwood-ci-clojure-1-11 {:dependencies [[org.clojure/clojure "1.11.0-alpha3"]
+                                           [org.clojure/spec.alpha "0.3.214"]]}}
