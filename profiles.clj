f23c982a9100415f4c4e7f86dfe80b9d5eec22e5
vemv <vemv@users.noreply.github.com>
2022-07-28 05:22:50 +0200

    Bump Eastwood

--- profiles.clj
+++ profiles.clj
@@ -2,9 +2,9 @@
                                                  [lein-pprint "1.1.2"]
                                                  [lein-subscribable-urls "0.1.0-alpha2"]
                                                  [threatgrid/lein-lean "0.6.0"]
-                                                 [jonase/eastwood "0.9.6"]
-                                                 [lein-lein "0.2.0"]]
-                      :dependencies             [[jonase/eastwood "0.9.6"]]
+                                                 [lein-lein "0.2.0"]
+                                                 [jonase/eastwood "1.2.4"]]
+                      :dependencies             [[jonase/eastwood "1.2.4"]]
                       :jvm-opts                 [;; Disable all UI features for disabling the clipboard - for personal security:
                                                  "-Djava.awt.headless=true"
                                                  ;; Remove useless icon from the Dock:
