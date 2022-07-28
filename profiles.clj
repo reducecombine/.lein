df77d12e64fe282e263243b55a21837075a0bdc3
vemv <vemv@users.noreply.github.com>
2022-07-28 05:15:29 +0200

    Discard `-Xverify:none`

--- profiles.clj
+++ profiles.clj
@@ -41,8 +41,6 @@
                                                  ;; Set a minimum amount of memory that matches/exceeds a given repl's initial usage (including various tooling).
                                                  ;; This way, initial performance improves:
                                                  "-Xms6G"
-                                                 ;; Improves startup time:
-                                                 "-Xverify:none"
                                                  ;; Enable various optimizations, for guaranteeing accurate benchmarking (at the cost of slower startup):
                                                  "-server"]
                       :monkeypatch-clojure-test false}
